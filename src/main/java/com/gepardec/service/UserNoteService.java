ad417c221d120c18aea74d2295782f7d098f6a82
Tod Oliver <olivertod11@yahoo.de>
2022-07-15 11:43:39 +0200

    Implement service

---
+++ src/main/java/com/gepardec/service/UserNoteService.java
@@ -0,0 +1,39 @@
+package com.gepardec.service;
+
+import com.gepardec.model.Note;
+import com.gepardec.model.User;
+import com.gepardec.repository.NoteRepository;
+import com.gepardec.repository.UserRepository;
+
+import javax.enterprise.context.RequestScoped;
+import javax.inject.Inject;
+import java.util.List;
+
+@RequestScoped
+public class UserNoteService {
+
+    @Inject
+    UserRepository userRepository;
+
+    @Inject
+    NoteRepository noteRepository;
+
+    public User getUserByUsername(String username) {
+        return userRepository.getUser(username);
+    }
+
+    public List<Note> getAllNotesForUser(String username) {
+        User user = userRepository.getUser(username);
+        return noteRepository.findAllNotesForUser(user);
+    }
+
+    public User createUser(User user) {
+        userRepository.persist(user);
+        return userRepository.find(user.getName()).firstResult();
+    }
+
+    public Note createNote(Note note) {
+        noteRepository.persist(note);
+        return noteRepository.findById(note.getId());
+    }
+}
